0b5fb6ec4c6d481ea92bc5486df59c664056630d
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-05-27 22:40:23 -0600

    G.   Write code to include validation to enforce the inputs needed by the Angular front-end.

--- src/main/java/edu/wgu/georgievl/d288/entities/Customer.java
+++ src/main/java/edu/wgu/georgievl/d288/entities/Customer.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.Set;
 
@@ -21,12 +23,24 @@ public class Customer {
     private Long id;
 
     @Column(name = "customer_first_name")
+    @NotBlank(message = "First Name can't be blank")
+    @Size(min = 3, max = 30, message = "First Name should be between 3 and 30 symbols")
     private String firstName;
 
     @Column(name = "customer_last_name")
+    @NotBlank(message = "Last name can't be blank")
+    @Size(min = 3, max = 30, message = "Last Name should be between 3 and 30 symbols")
     private String lastName;
+
+    @NotBlank(message = "Address can't be blank")
+    @Size(min = 5, max = 50, message = "Address should be between 5 and 50 symbols")
     private String address;
+
+    @NotBlank(message = "Postal code can't be blank")
+    @Size(min = 5, max = 5, message = "Postal code has to be 5 symbols long")
     private String postalCode;
+
+    @NotBlank(message = "Phone Number can't be blank")
     private String phone;
 
     @CreationTimestamp
